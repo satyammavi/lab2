aad9d0bba944c6829c1915faf6d81638fcf32dc2
Satyam mavi <124493832+satyammavi@users.noreply.github.com>
2026-02-24 14:58:25 +0530

    Create 100-String_Compression.py

---
+++ 100_problem_24/100-String_Compression.py
@@ -0,0 +1,17 @@
+text = input("Enter string: ")
+
+result = ""
+
+count = 1
+
+for i in range(len(text)-1):
+
+    if text[i] == text[i+1]:
+        count += 1
+    else:
+        result = result + text[i] + str(count)
+        count = 1
+
+result = result + text[-1] + str(count)
+
+print(result)
